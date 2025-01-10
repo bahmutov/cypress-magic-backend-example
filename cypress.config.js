dbcb7c73d3a63211a16da9d04c1fb57f64d01665
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-01-10 17:19:30 -0500

    latest plugin version

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  defaultBrowser: 'electron',
   env: {
     // https://github.com/bahmutov/cypress-magic-backend
     magicBackend: {

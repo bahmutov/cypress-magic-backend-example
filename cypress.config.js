801289caa98215dfa7c3245bd4c18b67e081695c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-01-25 11:17:23 -0500

    add plugin config

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('cypress')
+// https://github.com/bahmutov/cypress-magic-backend
+const registerMagicBackend = require('cypress-magic-backend/src/plugin')
 
 module.exports = defineConfig({
   defaultBrowser: 'electron',
@@ -27,8 +29,10 @@ module.exports = defineConfig({
     baseUrl: 'http://localhost:3000',
     fixturesFolder: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      // and load any plugins that require the Node environment
+      registerMagicBackend(on, config)
+      // IMPORTANT: return the config object
+      // because it might be modified by the plugin function
+      return config
     },
   },
 })

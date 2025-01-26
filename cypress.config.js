f6833560eb54027c5ecd5190bbb19e189531989e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-01-26 09:07:40 -0500

    add watch and reload plugin (#2)
    * add reload and use 127.0.0.1 explicitly
    
    * move the config into e2e block

--- cypress.config.js
+++ cypress.config.js
@@ -4,32 +4,41 @@ const registerMagicBackend = require('cypress-magic-backend/src/plugin')
 
 module.exports = defineConfig({
   defaultBrowser: 'electron',
-  env: {
-    // https://github.com/bahmutov/cypress-magic-backend
-    magicBackend: {
-      // this app makes "XHR" calls to load and update "/todos"
-      // match calls like
-      // GET /todos
-      // POST /todos
-      // DELETE /todos/1234
-      apiCallsToIntercept: [
-        {
-          method: '+(GET|POST)',
-          pathname: '/todos',
-        },
-        {
-          method: 'DELETE',
-          pathname: '/todos/*',
-        },
-      ],
-    },
-  },
   e2e: {
     // baseUrl, etc
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://127.0.0.1:3000',
     fixturesFolder: false,
+    env: {
+      // https://github.com/bahmutov/cypress-magic-backend
+      magicBackend: {
+        // this app makes "XHR" calls to load and update "/todos"
+        // match calls like
+        // GET /todos
+        // POST /todos
+        // DELETE /todos/1234
+        apiCallsToIntercept: [
+          {
+            method: '+(GET|POST)',
+            pathname: '/todos',
+          },
+          {
+            method: 'DELETE',
+            pathname: '/todos/*',
+          },
+        ],
+      },
+
+      // list the files and file patterns to watch
+      // https://github.com/bahmutov/cypress-watch-and-reload
+      'cypress-watch-and-reload': {
+        watch: ['index.html', 'app.js'],
+      },
+    },
     setupNodeEvents(on, config) {
       registerMagicBackend(on, config)
+
+      require('cypress-watch-and-reload/plugins')(on, config)
+
       // IMPORTANT: return the config object
       // because it might be modified by the plugin function
       return config

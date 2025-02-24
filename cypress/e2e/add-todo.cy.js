deda555ed81b99738a3676f860ca0237aa6ec435
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-02-23 20:37:02 -0500

    testing a3

--- cypress/e2e/add-todo.cy.js
+++ cypress/e2e/add-todo.cy.js
@@ -1,7 +1,11 @@
 /// <reference types="cypress" />
 
 beforeEach(() => {
-  cy.request('POST', '/reset', { todos: [] })
+  const mode = Cypress.env('magic_backend_mode')
+  if (mode !== 'playback') {
+    mode && cy.log(`during the test the mode is "${mode}"`)
+    cy.request('POST', '/reset', { todos: [] })
+  }
 })
 
 it('adds a todo', () => {
